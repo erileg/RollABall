9c39475c190bbf1b7d946af88978b34e05a7c472
erle <erik.legenhausen@gmail.com>
2018-05-05 13:55:06 +0200

    first commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    private float speed;
+
+	void Start()
+	{
+        speed = Random.Range(1.0f, 1.5f);
+        Debug.Log(gameObject.name + " -> speed: " + speed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * speed);
+	}
+}
