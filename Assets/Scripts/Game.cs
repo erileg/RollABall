e2d6d499ea6e723335fa5c809a955e9ad6483560
erle <erik.legenhausen@gmail.com>
2018-05-05 14:23:43 +0200

    added bird cam

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
+    public Camera mainCam;
+    public Camera birdCam;
+
 
 	// Use this for initialization
 	void Start () {
         Cursor.visible = false;
 	}
-	
+
+	void Update()
+	{
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            mainCam.enabled = !mainCam.enabled;
+            birdCam.enabled = !birdCam.enabled;
+        }	
+	}
+
 }
