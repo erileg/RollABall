70cb75c834b6d6fccb7ecd688608a2b6afcab089
Erik Legenhausen <erik.legenhausen@engram.de>
2018-05-07 13:27:45 +0200

    PickUpController renamed again

---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    private float speed;
+
+    void Start()
+    {
+        speed = Random.Range(1.0f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * speed);
+    }
+}
