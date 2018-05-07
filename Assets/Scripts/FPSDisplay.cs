134764ef35c404f722463c108053bee2e89a864e
erle <erik.legenhausen@gmail.com>
2018-05-07 21:03:47 +0200

    added skybox

--- Assets/Scripts/FPSDisplay.cs
+++ Assets/Scripts/FPSDisplay.cs
@@ -19,7 +19,7 @@ public class FPSDisplay : MonoBehaviour
         Rect rect = new Rect(-10, 10, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperRight;
         style.fontSize = h * 2 / 100;
-        style.normal.textColor = Color.green;
+		style.normal.textColor = Color.yellow;
         float msec = deltaTime * 1000.0f;
         float fps = 1.0f / deltaTime;
         string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
