cf8cd478cca762b080a4bcee9c31bb170fba055a
Erik Legenhausen <erik.legenhausen@engram.de>
2018-05-11 14:54:29 +0200

    added touch support

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
 
+    private Vector3 startPos, endPos, direction;
+    private float touchTimeStart, touchTimeEnd, timeInterval;
+
     private Rigidbody rb;
     private GameController gameController;
 
@@ -18,10 +21,28 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        
+        if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            touchTimeStart = Time.time;
+            var touchPos = Input.GetTouch(0).position;
+            startPos = new Vector3(touchPos.x, 0, touchPos.y);
+        }
 
-        Camera currentCam = Camera.current;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            touchTimeEnd = Time.time;
+            var touchPos = Input.GetTouch(0).position;
+            endPos = new Vector3(touchPos.x, 0, touchPos.y);
+            timeInterval = touchTimeEnd - touchTimeStart;
+            direction = endPos - startPos;
+            rb.AddForce(direction * direction.magnitude * .002f);
+        }
+
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var currentCam = Camera.current;
         if (currentCam)
         {
             Vector3 movement = currentCam.transform.TransformDirection(new Vector3(moveHorizontal, 0.0f, moveVertical));
